f1c661a5345cd4a69d1eaa104e1aba8d206a037c
Gerald Urschitz <gerald.urschitz@ready2order.com>
2020-10-20 00:37:53 +0200

    use formatjs intl pkg

--- helpers/formatters.ts
+++ helpers/formatters.ts
@@ -1,7 +1,18 @@
 import { LOCALE } from "./constants";
+import { createIntl, createIntlCache } from "@formatjs/intl";
+
+// This is optional but highly recommended
+// since it prevents memory leak
+const cache = createIntlCache();
+
+const intl = createIntl(
+  {
+    locale: "de-DE",
+    messages: {},
+  },
+  cache
+);
 
 export function formatNumber(value: number | bigint, precision = undefined) {
-  return Intl.NumberFormat(LOCALE, { maximumFractionDigits: precision }).format(
-    value
-  );
+  return intl.formatNumber(value);
 }
